940b6696c4ae0fa12090ae5aea974d25ff11b54f
Samir Lohar <samirlohar2403@gmail.com>
2025-08-15 14:25:15 +0530

    Time: 0 ms (100%), Space: 68.4 MB (14.51%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int PeakIndexInMountainArray(int[] arr) {
+        int n=arr.Length;
+        int left=0;
+        int right=n-1;
+        int ans=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(arr[mid+1]>arr[mid]){
+                 ans=mid+1;
+                 left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+}
