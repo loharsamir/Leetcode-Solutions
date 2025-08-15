37ab86b68804b4165623b9b8e8b8f3130abfb2ce
Samir Lohar <samirlohar2403@gmail.com>
2025-08-15 14:26:36 +0530

    Time: 0 ms (100%), Space: 68.4 MB (23.83%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cs
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cs
@@ -7,12 +7,12 @@ public class Solution {
         while(left<=right){
             int mid=left+(right-left)/2;
             if(arr[mid+1]>arr[mid]){
-                 ans=mid+1;
+                 //ans=mid+1;
                  left=mid+1;
             }else{
                 right=mid-1;
             }
         }
-        return ans;
+        return left;
     }
 }
