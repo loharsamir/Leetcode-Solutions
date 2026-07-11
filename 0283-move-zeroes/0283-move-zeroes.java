12b0c068b68178e09dfe70d857c53b032a0b1bcd
Samir Lohar <samirlohar2403@gmail.com>
2026-07-11 11:03:47 +0530

    Time: 2 ms (92.23%), Space: 48.1 MB (6.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int index=0;
+        int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[index]=nums[i];
-                index++;
-                
+                nums[j]=nums[i];
+                j++;
             }
         }
-        while(index<nums.length){
-            nums[index++]=0;
+        for(int i=j;i<nums.length;i++){
+            nums[i]=0;
         }
     }
 }
